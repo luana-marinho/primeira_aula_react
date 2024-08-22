c50d8637a4d2218125fcf228e327db4a9bd801c7
Luana <luana.gabriela96@hotmail.com>
2024-08-22 15:29:19 -0300

    Criação projeto React

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './pages/home/Home'
+import Contador from './pages/contador/Contador'
+import Tarefa from './pages/tarefa/Tarefa'
+
+function App() {
+
+ 
+
+  return (
+    <>
+       {/* <Home 
+        titulo = "Minha primeira postagem"
+        texto = "Texto da minha primeira postagem"/>
+    
+    <Home 
+        titulo = "Minha segunda postagem"
+        texto = "Texto da minha segunda postagem"/>*/}
+    {/* <Contador/> */}
+    <Tarefa/> 
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/contador/Contador.tsx
@@ -0,0 +1,27 @@
+﻿import { useState } from "react";
+
+function Contador() {
+
+   //const [nome_estado, função_set_state] = useState(valor_inicial_do_estado)
+
+   const [valor, setValor] = useState(0);
+
+   function handleClick(){
+      setValor(valor +1);
+
+   }
+
+  return (
+    <div>
+      
+      <h2>Componente Contador</h2>
+
+      <p>O valor atual do meu estado é: {valor}</p>
+
+      <button onClick={handleClick}>Adicionar  +1</button>
+      
+    </div>
+  )
+}
+
+export default Contador
